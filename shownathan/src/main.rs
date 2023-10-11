5637660a0d9a0dc670eb9dd2d03f8398e780f50f
aaronaskew <aaronaskew@gmail.com>
2023-10-11 16:05:22 -0500

    fixed warnings

--- shownathan/src/main.rs
+++ shownathan/src/main.rs
@@ -1,13 +1,11 @@
 fn main() {
     println!("Hello, world!");
 
-    let mut x: u8 = 4;
+    let x: u8 = 4;
 
-    let s = String::from("shoebox");
+    let _s = String::from("shoebox");
 
     println!("x={}", x);
 }
-
-fn add_to_string(s:String) {
-    
-}
+#[allow(dead_code)]
+fn add_to_string(_s: String) {}
