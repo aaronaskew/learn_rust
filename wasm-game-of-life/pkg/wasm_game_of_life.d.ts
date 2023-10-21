f778c258e18f6cdb1a7023513b3b6b459a655070
Aaron Askew <aaronaskew@gmail.com>
2023-10-21 08:24:48 -0500

    un-ignored the pkg results

---
+++ wasm-game-of-life/pkg/wasm_game_of_life.d.ts
@@ -0,0 +1,34 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} s
+*/
+export function log_to_console(s: string): void;
+/**
+*/
+export class Universe {
+  free(): void;
+/**
+*/
+  tick(): void;
+/**
+* @returns {Universe}
+*/
+  static new(): Universe;
+/**
+* @returns {string}
+*/
+  render(): string;
+/**
+* @returns {number}
+*/
+  width(): number;
+/**
+* @returns {number}
+*/
+  height(): number;
+/**
+* @returns {number}
+*/
+  cells(): number;
+}
